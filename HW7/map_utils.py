dfcefedf9bd15c87aae4dd99b2ee3e293ed2ecff
Catherine Kuhn <ckuhn@uw.edu>
2016-02-24 15:24:29 -0800

    added in my code for which the HW7 pull request will be based on

---
+++ HW7/map_utils.py
@@ -0,0 +1,73 @@
+
+# This code currently doesn't work because the ee.mapclient dependency is broken
+import ee
+import ee.mapclient
+import datetime
+
+
+# Authenticate your user credentials so you can access the API
+ee.Initialize()
+
+# Center the map on the study area.
+ee.mapclient.centerMap(-93, 40, 4)
+
+
+"""In this step, you will select rows from a fusion table. 
+This will allow you to display on a map feature data. 
+The feature data is sort of like a mappable array composed of 
+thousands of points, each with a lat/long 
+and many associated variables. """
+
+# Select the 'Sonoran desert' feature from the TNC Ecoregions fusion table.
+fc1 = (ee.FeatureCollection('ft:1Ec8IWsP8asxN-ywSqgXWMuBaxI6pPaeh6hC64lA')
+      .filter(ee.Filter().eq('ECO_NAME', 'Sonoran desert')))
+
+# Select the USGS feature collection of sampling data from Summer 2015
+fc2 = ee.FeatureCollection("ft:1KLL3aOt7-mavHuL_uyLLPXOf7vUVk6v08XbzIepq");
+
+# Paint it into a blank image.
+image1 = ee.Image(0).mask(0)
+ee.mapclient.addToMap(image1.paint(fc, 0, 5))
+
+image2 = ee.Image(0).mask(0)
+ee.mapclient.addToMap(image2.paint(fc, 0, 5))
+
+
+
+""" In this step you are going to create a background map using Landsat imagery
+by filtering an image collection by date to make a median composite. The composite
+is composed of the stack of all the individual Landsat images from the entire date range.
+Each image also has many bands and each band represents different kinds of spectral information 
+collected by the instrument. 
+"""
+
+# Import Landsat 8 satellite imagery
+var L8 = ee.ImageCollection("LANDSAT/LC8_L1T")   
+
+# Filter to only include images within the watershed boundaries.
+polygon = ee.Feature.Polygon([[
+    [-109.05, 37.0], [-102.05, 37.0], [-102.05, 41.0],   
+    [-109.05, 41.0], [-111.05, 41.0], [-111.05, 42.0],   
+    [-114.05, 42.0], [-114.05, 37.0], [-109.05, 37.0]]])
+
+# Create a Landsat 8 composition for the Summer of 2015 and filter
+# by the bounds of the feature collection 
+var collection = ee.Algorithms.Landsat.simpleComposite({
+  collection: L8.filterDate('2015-7-1', '2015-8-16'),
+  asFloat: true});
+
+# Select the median pixel.
+image1 = collection.median()
+
+# Select the red, green and blue bands.
+image = image1.select('B3', 'B2', 'B1')
+ee.mapclient.addToMap(image, {'gain': [1.4, 1.4, 1.1]})
+
+
+
+
+
+
+
+
+
