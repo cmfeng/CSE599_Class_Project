3ed15d9478204be9b61ff5552442f7a61bf69875
Will Gagne-Maynard <will.maynard@gmail.com>
2016-02-26 13:33:16 -0800

    final update to utils

--- HW7/data_cleaning_utils.py
+++ HW7/data_cleaning_utils.py
@@ -2,20 +2,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Importing the data.
-#This step is key, as we have to ensure that the time format 
-#of the data is recognized
+"""
+Importing the data.
+This step is key, as we have to ensure that the time format 
+of the data is recognized
+"""
+
 def import_data():
-    raw_data = pd.read_csv(data.csv, parse_dates=True, 
+    datetime_column = input("datetime column name? ")
+    raw_data = pd.read_csv("All Upstream.csv", parse_dates=True, 
         infer_datetime_format=True)
     raw_data[datetime_column] = pd.to_datetime(raw_data[datetime_column])
     raw_data.index = raw_data[datetime_column]
     #Need to add code to catch errors and ensure this is in datetime format
     return raw_data
 
-#Defining the first function to smooth the data
-#This will return the dataframe with the smoothed data overwriting the raw
-#it will also return a plot of the smoothed data overlaid on the raw data
+"""
+Defining the first function to smooth the data
+This will return the dataframe with the smoothed data overwriting the raw
+it will also return a plot of the smoothed data overlaid on the raw data
+"""
 def smooth_data(column):
     data = import_data()
     #This asks the user for what size window to average over
@@ -32,9 +38,11 @@ def smooth_data(column):
     data[column] = moving
     return data
 
-#Defining the function to reuce the size of the data
+"""
+Defining the function to reuce the size of the data
+"""
 def reducer():
-    data = smooth_data()
+    data = smooth_data("pH")
     freq = input("What frequency would you like to resample to? Format = XS(seconds), XT(minutes)")
     #This will resample the data.  If downsampling, it will take the mean of the
     #points.  If upsampling, it will fill backwards
@@ -42,7 +50,6 @@ def reducer():
 
     print(data.size)
     print(resampled.size)
-
     return resampled
 
 
